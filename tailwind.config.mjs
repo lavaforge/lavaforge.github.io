e06534ac992df0ca1ab81db41cd77fc5527a79ee
Philipp Scheede <github@p-scheede.de>
2024-03-07 22:02:27 +0100

    build 'under construction' index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        glaze: '#fbf2e3',
+        stallion: '#081d25',
+      },
+      fontFamily: {
+        cinzel: ['Cinzel', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
